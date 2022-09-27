b1f14a06b3c96020a401958aadbf2ba8da187d34
R Farley <rfarley3@users.noreply.github.com>
2022-09-27 16:37:57 -0400

    PO Sync now on pin 1 shared with built-in LED, reorg and document

--- midi-to-po-analogue-sync.ino
+++ midi-to-po-analogue-sync.ino
@@ -23,7 +23,7 @@
  *     - consider mod with fractional duty cycle percentage (per pulse, only on 50%, with potential offset start by x%); or just be creative with envelope bit
  *   - CV (0-5V, Volt per Octave) with several options to configure, uses a gate and note algorithm setting
  *     - Depending on the options you enable/disable below you can change if the gate/note off turns off CV, and/or if there are retrigger events at note change or clock ticks
- *     - Depending on the option you chose for the note algorithm, you can do lowest, highest, latest
+ *     - Depending on the option you chose for the note algorithm, you can do lowest, highest, latest, loudest
  *     - You can choose arp (any > 1 notes; up, down, up-down, rand) to handle multiple notes, falling back to alg once all notes are off
  *     - Default is CV gated (by key press) glissando (ignore note on retriggers) (no clock retrigger), latest note, up-down arp
  */
@@ -44,14 +44,16 @@
 /* MIDI
  * Given this pin number use SoftwareSerial and MIDI.h to set up Rx
  * Do not set Tx to a pin that is used by other things
+ * You should use the datasheet optocoupler method:
+ * MIDI Vref (wire 4) ---|---[optocoupler]---- (pulled up) Rx (IN_D_MIDI)
+ *                       ^d1      |
+ * MIDI Data (wire 5) ---|--------|
+ * But if you don't care about your equipment (ground loops/voltage isolation), then you can power this from MIDI
+ * MIDI Vref ------------ Vcc (physical pin 8, marked 5V or 3V on Trinket)
+ * MIDI Data ------------ Rx (IN_D_MIDI)
+ * MIDI Gnd  ------------ Gnd (physical pin 4)
  */
-#define IN_D_MIDI 0  
-// Status LED
-#define OUT_D_BOARD_LED 1
-#define LED_BLINK_PERIOD 100
-unsigned long LED_BLINK_ON_MILLIS = 0;
-// CV output
-#define OUT_A_CV 1  // if not CV then OUT_D_GATE
+#define IN_D_MIDI 0
 /* PO/Volca/Analogue Sync V-trig pulse
  * Prev volca-po-analogue-sync-divider req minimum of 30 msec pulse and refactory period
  *   - 15 in the active state, 14 low/sleep, and loop had a 1 sleep (so loop could use lower power, only poll 1/msec)
@@ -61,16 +63,35 @@ unsigned long LED_BLINK_ON_MILLIS = 0;
  * PO may only be 2.5 msecs, and Volca is 15 msecs
  *   - PO and Volca rated for 5 V 15 msec v-trig pulse, but both should work with 3.3 V (would still give 3 and 5 V logic high)
  */
-#define OUT_D_PO_SYNC 2
+#define OUT_D_PO_SYNC 1  // on board LED is on pin 1
 #define SYNC_PULSE_PERIOD 15
 unsigned long SYNC_PULSE_ON_MILLIS = 0;
+/* Input
+ * Using Pin 2, which is dread 2, but aread 1
+ * Possible inputs:
+ *   manually adjust/offset beat tracking by n clocks
+ * - auto vs manual pulsing=true
+ *   LB frequency pulses per beat change (1/8, 1/4/, 1/2, 1, 2, 4, 8) default 1
+ *   LB sync duty cycle percentage, default 100%
+ *   CV channel select 0-15
+ * - CV note choice algorithm (latest, highest, lowest, loudest, arp up, down, up-down, rand), default latest
+ *   CV is only gate (aka force always 5v) vs PWM DAC as % of Octave per Volt by note number
+ *   CV is gated while note on, vs always on last setting that makes sense
+ *   CV is glissando vs s-triggers when a note is changed
+ *   CV is ignores clock vs s-triggers at LB pulses per beat
+ * #define IN_A_NOTEALG/CHAN 1  // if not CV then IN_A_FREQ/CHAN 2
+ * #define OUT_D_GATE_OFF 2  // if CV RC DAC circuit lingers beyond gate add RT switch with bias HIGH when gate is off, but loose IN_A_* 2
+ */
 // littleBits Sync (5v to 0v s-trig)
-#define OUT_D_LB_SYNC 3  // shared with USB, has pullup on it
-// #define            4 see aread in next line, aread 2 is dread 4
-// #define IN_A_NOTEALG/CHAN 2  // if not CV then IN_A_FREQ/CHAN 2
-// #define OUT_D_GATE_OFF 4  // if CV RC DAC circuit lingers beyond gate add RT switch with bias HIGH when gate is off, but loose IN_A_* 2
+#define OUT_D_LB_SYNC 3  // shared with USB, has 1.5k pullup on it per board spec
+#define LB_STRIG_PERIOD 15
+unsigned long LB_STRING_MILLIS = 0;
+// CV output
+//  Default CV is gated (by key press) glissando (ignore note on retriggers) (no clock retrigger), latest note, up-down arp
+#define OUT_A_CV 4  // if not CV then OUT_D_GATE
 
-// from Examples MIDI Bench
+
+// from MIDI Bench Example
 #if defined(ARDUINO_SAM_DUE) || defined(USBCON)
     // Print through USB and bench with Hardware serial
     MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, midiBench);
@@ -83,84 +104,37 @@ unsigned long SYNC_PULSE_ON_MILLIS = 0;
 #endif
 
 
-void handleClock(void);
-void handleStart(void);
-void handleContinue(void);
-void handleStop(void);
-void handleSystemReset(void);
-void pulse_beat(void);
-void pulse_po(void);
-void led_pin_on(void);
-void led_pin_off(void);
-void sync_pin_on(void);
-void sync_pin_off(void);
+void handleClock();
+void handleStart();
+void handleContinue();
+void handleStop();
+void handleSystemReset();
+void pulse_beat();
+void pulse_halfbeat();
+void sync_pin_on();
+void sync_pin_off();
 
 
 unsigned int clock_ticks = 0;
 bool pulsing = false;
 
 
-void setup() {
-  //setup_blink();
-  setup_v1();
-}
-
-
-void loop() {
-  //loop_blink();
-  loop_v1();
-}
-
 /*************************************************
  * read MIDO from d0
- * v0 is to blink clock sync on LED on d1, then only blink per beat
- * v1 blink beat on LED on d1, output PO on d2
- * v2 output LB sync as LED on d1; or output LB to d3
- * v3 blink clock sync on d1 on LED, output PO on d2, and LB sync on d3
- * v4 blink gate (note on/off) on LED on d1
- * v5 output gated, latest CV on d1
+ *   v0 is to blink clock sync on LED on d1 (built-in LED), then only blink per beat
+ *   v1 output PO on d1 (which will blink sync on built-in LED)
+ *   v2 output LB sync on d3
+ *   v3 blink gate (note on/off) on LED on d4
+ *   v4 output gated, latest CV on d4
  */
-
- void setup_blink() {
-  pinMode(OUT_D_BOARD_LED, OUTPUT);
-  digitalWrite(OUT_D_BOARD_LED, LOW);
-  pinMode(OUT_D_PO_SYNC, OUTPUT);
-  digitalWrite(OUT_D_PO_SYNC, LOW);
- }
-
-void loop_blink() {
-  unsigned long now = millis();
-  if (now % 1000 > 100 && now % 1000 < 200) {
-    digitalWrite(OUT_D_PO_SYNC, HIGH);
-  }
-  else {
-    digitalWrite(OUT_D_PO_SYNC, LOW);
-  }
-  if (now % 1000 < 100) {
-    digitalWrite(OUT_D_BOARD_LED, HIGH);
-    }
-  else {
-    digitalWrite(OUT_D_BOARD_LED, LOW);
-  }
-}
-
-void setup_v1() {
+void setup() {
+  // default Arduino named function called once, before loop
   clock_ticks = 0;
   pulsing = false;
-  pinMode(OUT_D_BOARD_LED, OUTPUT);
-  digitalWrite(OUT_D_BOARD_LED, LOW);
-  LED_BLINK_ON_MILLIS = 0;
   pinMode(OUT_D_PO_SYNC, OUTPUT);
   digitalWrite(OUT_D_PO_SYNC, LOW);
   SYNC_PULSE_ON_MILLIS = 0;
-  // IN_D_MIDI 
-  //MIDI_CREATE_DEFAULT_INSTANCE();
-  //MIDI.begin();           // Launch MIDI, by default listening to channel 1. // 31250
-  // initialize MIDI serial
-  //SoftwareSerial midiSerial(0, 2); // RX, TX
-  //MIDI_CREATE_INSTANCE(SoftwareSerial, midiSerial, polbmidi);
   // https://github.com/FortySevenEffects/arduino_midi_library/wiki/Using-Callbacks
-  // midiPoLb.setHandleNoteOn(handleNoteOn);
   midiPoLb.setHandleClock(handleClock);
   midiPoLb.setHandleStart(handleStart);
   midiPoLb.setHandleContinue(handleContinue);
@@ -169,22 +143,55 @@ void setup_v1() {
   midiPoLb.begin();
 }
 
-void pulse_beat(void) {
-  led_pin_on();
+
+void loop() {
+  // default Arduino named function called repeatedly
+  midiPoLb.read();  // blocks as long as it takes to handle any messages
+  sync_pin_off();
+}
+
+
+/* Helper functions
+ * Turning on and off pins, setting timers, global bools/flags, etc
+ */
+void pulse_beat() {
+  // called once per 24 MIDI clocks (once per beat)
+  // used to be where the built-in LED was blinked per beat, but now that pin is for PO sync so LED flashes twice per beat
   // s-trig LB sync
 }
 
-void pulse_po(void) {
+
+void pulse_half_beat() {
+  // called once per 12 MIDI clocks (half a beat)
   sync_pin_on();
 }
 
 
-void loop_v1() {
-    midiPoLb.read();
-    led_pin_off();
-    sync_pin_off();
+void sync_pin_on() {
+  // send a pulse on the PO/Volca/Analogue Sync out pin
+  digitalWrite(OUT_D_PO_SYNC, HIGH);
+  SYNC_PULSE_ON_MILLIS = millis();
 }
 
+
+void sync_pin_off() {
+  // polled each loop, if the pulse has been high long enough, turn it off
+  if (SYNC_PULSE_ON_MILLIS) {
+    // attiny millis overflow is ~49 days, logic to detect that condition out of scope for this sketch
+    if ((millis() - SYNC_PULSE_ON_MILLIS) > SYNC_PULSE_PERIOD) {
+      digitalWrite(OUT_D_PO_SYNC, LOW);
+      SYNC_PULSE_ON_MILLIS = 0;
+    }
+  }
+}
+
+
+/* MIDI Handlers
+ * Do any of these need to be covered?
+ *   - TimeCodeQuarterFrame (if 0, stop and start/set clock_ticks to 0)?
+ *   - SongPosition (if 0, stop and start, set clock_ticks to 0)?
+ *   - SongSelect (stop and start)?
+ */
 void handleClock(void) {
   if (!pulsing) {
     return;
@@ -193,20 +200,25 @@ void handleClock(void) {
   // There are 24 MIDI clocks per beat, so 24 MIDI PPQN
   // PO and Volca use 2 PPQN, so 12 MIDI pulses per 1 PO pulse
   if ((clock_ticks % 12) == 0) {
-    pulse_po();
+    //                   0         1         2
+    // clock_ticks (in)  0123456789012345678901234
+    // call pulse?       ynnnnnnnnnnnynnnnnnnnnnny
+    // clock_ticks (out) 1234567890123456789012341
+    pulse_half_beat();
   }
   // http://lauterzeit.com/arp_lfo_seq_sync/
-  //                   0         1         2
-  // clock_ticks (in)  0123456789012345678901234
-  // call pulse?       ynnnnnnnnnnnnnnnnnnnnnnny
-  // clock_ticks (out) 1234567890123456789012341
   if ((clock_ticks % 24) == 0) {
     // avoid eventual overflow
     clock_ticks = 0;
+    //                   0         1         2
+    // clock_ticks (in)  0123456789012345678901234
+    // call pulse?       ynnnnnnnnnnnnnnnnnnnnnnny
+    // clock_ticks (out) 1234567890123456789012341
     pulse_beat();
   }
   clock_ticks++;
 }
+
 
 void handleStart(void) {
   // It's uncertain if there should be a pulse on start, or only the 1st clock after a start
@@ -215,10 +227,12 @@ void handleStart(void) {
   pulsing = true;
 }
 
+
 void handleContinue(void) {
   // assume you can't pause, so continue is same as start for PO
   handleStart();
 }
+
 
 void handleStop(void) {
   // turn off any CV
@@ -226,48 +240,12 @@ void handleStop(void) {
   pulsing = false;
 }
 
+
 void handleSystemReset(void) {
   // system panic, turn off any notes
   handleStop();
-// TODO
-//  digitalWrite(OUT_D_BOARD_LED, LOW);
-//  pulse_millis[OUT_D_BOARD_LED] = 0;
-//  digitalWrite(OUT_D_PO_SYNC, LOW);
-//  pulse_millis[OUT_D_PO_SYNC] = 0;
+  // TODO
+  //  digitalWrite(OUT_D_PO_SYNC, LOW);
+  //  SYNC_PULSE_ON_MILLIS = 0;
   // TODO turn off any cv/notes/lb-sync
-}
-
-// TODO
-// TimeCodeQuarterFrame (if 0, stop and start/set clock_ticks to 0)?
-// SongPosition (if 0, stop and start, set clock_ticks to 0)?
-// SongSelect (stop and start)?
-
-void led_pin_on() {
-  digitalWrite(OUT_D_BOARD_LED, HIGH);
-  LED_BLINK_ON_MILLIS = millis();
-}
-
-void sync_pin_on() {
-  digitalWrite(OUT_D_PO_SYNC, HIGH);
-  SYNC_PULSE_ON_MILLIS = millis();
-}
-
-void led_pin_off() {
-  if (LED_BLINK_ON_MILLIS) {
-    // attiny millis overflow is ~49 days, logic to detect that condition out of scope for this sketch
-    if ((millis() - LED_BLINK_ON_MILLIS) > LED_BLINK_PERIOD) {
-      digitalWrite(OUT_D_BOARD_LED, LOW);
-      LED_BLINK_ON_MILLIS = 0;
-    }
-  }
-}
-
-void sync_pin_off() {
-  if (SYNC_PULSE_ON_MILLIS) {
-    // attiny millis overflow is ~49 days, logic to detect that condition out of scope for this sketch
-    if ((millis() - SYNC_PULSE_ON_MILLIS) > SYNC_PULSE_PERIOD) {
-      digitalWrite(OUT_D_PO_SYNC, LOW);
-      SYNC_PULSE_ON_MILLIS = 0;
-    }
-  }
 }
