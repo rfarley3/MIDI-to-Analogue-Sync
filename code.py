ccb0549a8409b41749743ffd44f26e8cd033b05b
R Farley <rfarley3@users.noreply.github.com>
2022-11-03 14:58:49 -0400

    Use docs guide of code.py vs main.py, blink led for initial board test

---
+++ code.py
@@ -0,0 +1,92 @@
+# MIDI to Pocket Operator Sync (a form of Analogue Sync)
+"""MIDI to Pocket Operator Sync (a form of Analogue Sync).
+See README.md and midi-to-po-analogue-sync.ino for more docs
+
+- verify current meets requirements, 1 led blink, then dotstar color
+- port code from ino
+ * read MIDI from d0
+ *   v0 is to blink clock sync on LED on ?d1? (built-in LED), then only blink per beat
+ *   v1 output PO on ?d1?
+ *   v2 output LB sync on ?d3?
+ *   v3 blink gate (note on/off) on LED on ?d4?
+ *   v4 output gated, latest CV on ?d4?
+"""
+import board
+from digitalio import DigitalInOut, Direction, Pull
+# from analogio import AnalogOut, AnalogIn
+# import touchio
+# from adafruit_hid.keyboard import Keyboard
+# from adafruit_hid.keycode import Keycode
+#import adafruit_dotstar as dotstar
+import time
+
+# One pixel connected internally!
+#dot = dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1, brightness=0.2)
+
+# Built in red LED
+led = DigitalInOut(board.D13)
+led.direction = Direction.OUTPUT
+
+# Analog input on D0
+# analog1in = AnalogIn(board.D0)
+
+# Analog output on D1
+# aout = AnalogOut(board.D1)
+
+# Digital input with pullup on D2
+# button = DigitalInOut(board.D2)
+# button.direction = Direction.INPUT
+# button.pull = Pull.UP
+
+# Capacitive touch on D3
+# touch = touchio.TouchIn(board.D3)
+
+
+######################### HELPERS ##############################
+
+# Helper to convert analog input to voltage
+def getVoltage(pin):
+    return (pin.value * 3.3) / 65536
+
+# Helper to give us a nice color swirl
+def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if (pos < 0):
+        return (0, 0, 0)
+    if (pos > 255):
+        return (0, 0, 0)
+    if (pos < 85):
+        return (int(pos * 3), int(255 - (pos*3)), 0)
+    elif (pos < 170):
+        pos -= 85
+        return (int(255 - pos*3), 0, int(pos*3))
+    else:
+        pos -= 170
+        return (0, int(pos*3), int(255 - pos*3))
+
+######################### MAIN LOOP ##############################
+
+i = 0
+while True:
+    # spin internal LED around! autoshow is on
+    # dot[0] = wheel(i & 255)
+
+    # set analog output to 0-3.3V (0-65535 in increments)
+    # aout.value = i * 256
+
+    # Read analog voltage on D0
+    # print("D0: %0.2f" % getVoltage(analog1in))
+
+    # use D3 as capacitive touch to turn on internal LED
+    # if touch.value:
+    #     print("D3 touched!")
+
+    # if not button.value:
+    #     print("Button on D2 pressed!")
+
+    if (i % 32) < 16:
+        led.value = False
+    else:
+        led.value = True
+    i = (i+1) % 256  # run from 0 to 255
